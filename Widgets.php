d729efd057fc9e433f27ba7ed1ef5a7c3ef2b3e4
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-20 14:28:36 +1100

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Fintech\Components\Test;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
